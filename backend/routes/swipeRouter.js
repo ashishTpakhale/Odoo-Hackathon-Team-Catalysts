d8469b52d807a634d48a3cc862e2dc30f3030bdf
Ashish Pakhale <145212408+ashishTpakhale@users.noreply.github.com>
2025-07-12 16:49:31 +0530

    Delete backend directory

--- backend/routes/swipeRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getPotentialMatches, swipeAction} = require('../controllers/swipeController');
-const {authCheck} = require('../middlewares/authCheck');
-
-router.get('/', authCheck, getPotentialMatches)
-router.post('/', authCheck, swipeAction)
-
-module.exports = router;
--- backend/utils/detokenizer.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-function detokenize(token){
-    const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
-    return decodedToken
-}
-
-module.exports = detokenize
