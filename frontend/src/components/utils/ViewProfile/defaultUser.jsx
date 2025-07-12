7849090f01c5c46d9a2546eac3cc2599c165d290
unmesh-varade <unmeshvarade.official@gmail.com>
2025-07-12 16:48:34 +0530

    Delete frontend directory

--- frontend/src/components/utils/ViewProfile/defaultUser.jsx
+++
@@ -1,8 +0,0 @@
-export const defaultUser = {
-    fname: "DEFAULT_FNAME",
-    lname: "DEFAULT_LNAME",
-    username: "DEFAULT_USERNAME",
-    bio: "DEFAULT_BIO",
-    skills: [],
-    interests: []
-}
